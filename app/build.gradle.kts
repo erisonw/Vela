ef1dd71c12a0773edf1a513ace216caaf0c6f054
erisonw <erisonwang1220@gmail.com>
2026-07-06 23:38:58 +0800

    build(release): enable shrinker release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,17 @@ android {
         compose = true
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
